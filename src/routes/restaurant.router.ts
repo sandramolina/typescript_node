f9fcedf2e5b208aedc2d24cbdec62435ca7d729f
Sandra Molina Palencia <smolinapalencia@gmail.com>
2023-06-17 18:58:34 +0100

    adds route to get resto by id

--- src/routes/restaurant.router.ts
+++ src/routes/restaurant.router.ts
@@ -1,15 +1,14 @@
 // External Dependencies
 import express, { Request, Response } from 'express';
-// import { ObjectId } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import { collections } from '../services/database.service';
-// import Restaurant from '../models/restaurant';
 
 // Global Config
 export const restaurantsRouter = express.Router();
 
 restaurantsRouter.use(express.json());
 
-// GET
+// GET all restaurants
 restaurantsRouter.get('/restaurants', async (_req: Request, res: Response) => {
   try {
     const restaurants = await collections.restaurants.find({}).toArray();
@@ -18,6 +17,28 @@ restaurantsRouter.get('/restaurants', async (_req: Request, res: Response) => {
     res.status(500).send(error.message);
   }
 });
+
+// GET restaurants by ID
+restaurantsRouter.get(
+  '/restaurants/:id',
+  async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+      const query = { _id: new ObjectId(id) };
+      const restaurant = await collections.restaurants.findOne(query);
+
+      if (restaurant) {
+        res.status(200).send(restaurant);
+      }
+    } catch (error) {
+      res
+        .status(404)
+        .send(`Unable to find matching document with id: ${req.params.id}`);
+    }
+  }
+);
+
 // POST
 
 // PUT
